bf3099ae01073633955107469d9dcdfb7025346d
Raúl Osuna <raulosuna@gmail.com>
2022-03-30 00:23:31 +0200

    Add Procfile, change callbackURL in passport.use method to use url from Heroku

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://raulillo82-secrets.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
