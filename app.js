dafc5be108d5effdf676fe7a47aa67137ac7ad72
Raúl Osuna <raulosuna@gmail.com>
2022-03-29 23:32:58 +0200

    Add changes for using in the cloud with Heroku

--- app.js
+++ app.js
@@ -174,6 +174,10 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function (){
+  console.log("Server started successfully");
 });
